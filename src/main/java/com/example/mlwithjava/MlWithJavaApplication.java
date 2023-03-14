c6023db40e5957972cfed4f0a2ed126a313a75c3
bbring2 <jennachoi27@gmail.com>
2023-03-14 16:46:35 +0900

    Initial commit

---
+++ src/main/java/com/example/mlwithjava/MlWithJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.mlwithjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MlWithJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MlWithJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mlwithjava/MlWithJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mlwithjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlWithJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
